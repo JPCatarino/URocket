6714eea6a01f44a603b4c738d973d70b4a2467d1
JPCatarino <jorge.catarino@ua.pt>
2019-09-07 12:52:37 +0100

    CHANGED req functions to call functions

--- server/functions/index.js
+++ server/functions/index.js
@@ -3,23 +3,32 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-exports.addNewRocket = functions.https.onRequest(async (req, res) => {
+exports.addNewRocketC = functions.https.onRequest(async (req, res) => {
     // const owner = req.query.uid;
     // const name = req.query.name;
     
-    let data = req.body 
+    let data = req.body; 
 
     if(req.method !== "POST"){
         res.status(400).send('Please send a POST request');
         return;
        }
     
-    let reference = admin.firestore().doc('users/' + data.owner)
+    let reference = admin.firestore().doc('users/' + data.owner);
        
     admin.firestore().collection('rockets').add({owner: reference, name: data.name, is_public: data.is_public}).then(function(){res.send('OK').status(200)});
 });
 
-exports.addNewFlight = functions.https.onRequest(async (req, res) => {
+exports.addNewRocket = functions.https.onCall((data) => {
+    // const owner = req.query.uid;
+    // const name = req.query.name;
+    
+    let reference = admin.firestore().doc('users/' + data.owner);
+       
+    admin.firestore().collection('rockets').add({owner: reference, name: data.name, is_public: data.is_public}).then(function(){return {res:'OK'}});
+});
+
+exports.addNewFlightC = functions.https.onRequest(async (req, res) => {
     let data = req.body;
 
     if(req.method !== "POST"){
@@ -29,6 +38,11 @@ exports.addNewFlight = functions.https.onRequest(async (req, res) => {
 
     // res.send(data[1]._id);
     admin.firestore().collection('flights').add({launch_timestamp: data.launch_timestamp, rocket: admin.firestore().doc('rockets/' + data.rocket), telemetry: data.telemetry}).then(function(){res.send('OK').status(200)});
+});
+
+exports.addNewFlight = functions.https.onCall((data) => {
+    // res.send(data[1]._id);
+    admin.firestore().collection('flights').add({launch_timestamp: data.launch_timestamp, rocket: admin.firestore().doc('rockets/' + data.rocket), telemetry: data.telemetry}).then(function(){return {res:'OK'}});
 });
 
 exports.addNewUser = functions.auth.user().onCreate((user) => {
